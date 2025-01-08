908fae87a3f9b97b3e9d3c5e8a65838a8ee7b5ee
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-08 10:48:18 +0300

    money func

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -2,3 +2,16 @@ import React, { useState } from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import "../css/currency.css";
 import axios from "axios";
+
+function Currency() {
+	const [amount, setAmount] = useState(0);
+	const [fromCurrency, setFromCurrency] = useState("USD");
+	const [toCurrency, setToCurrency] = useState("TRY");
+	const [result, setResult] = useState("");
+
+
+
+			
+	let token = "fca_live_FXhn90pUyFbfVTdrOWM2D7FM3b9wcfnXrzIYN0tO";
+	let baseUrl = "https://api.freecurrencyapi.com/v1/latest";
+    
